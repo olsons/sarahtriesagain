8314e49263288ac5cb203afb98d495a8201c192a
Sarah Elizabeth Olson <solson3@student03.cse.nd.edu>
2016-04-13 13:59:26 -0400

    Hey

--- main.cpp
+++ main.cpp
@@ -7,7 +7,31 @@ using namespace std;
 int main(){
 	int answer=0; //customer choice for the program
 	int country=5;
-	string countryName;
+	//All of the instantiations of the city classes
+	cityClass Salzburg("German","Austria",145871, 5, 9, 7, 5, 9);
+	cityClass Vienna("Austria", "German", 1741000, 8, 4, 7, 7, 7);
+	cityClass Venice("Italy", "Italian", 260060, 6,2,6,6,9);
+	cityClass Tuscany("Italy", "Italian", 3753000, 7,3,3,2,9);
+	cityClass StPetersburg("Russia", "Russian", 249688, 3, 5, 8, 8, 8);
+	cityClass Stockholm("Sweden", "Swedish", 789024, 4,5,7,6,7);
+	cityClass Rome("Italy", "Italian", 2626000, 4, 5, 9, 10, 7);
+	cityClass Prague("Czech Republic", "Czech", 1247000, 9,8,7,6,8);
+	cityClass Paris("France", "French", 66030000, 2,6,9,10,9);
+	cityClass Monaco("France", "French", 37831, 5, 6, 5, 3, 6);
+	cityClass Madrid("Spain", "Spanish", 3165000, 8,6, 7, 8,8);
+	cityClass London("United Kingdom", "English", 8539000, 3, 6, 8,7,7);
+	cityClass Lisbon("Portugal", "Portuguese", 530847, 8,7,6,5,7);
+	cityClass Istanbul("Turkey", "Turkish", 14030000, 8,8,8,7,7);
+	cityClass Florence("France", "French", 361679, 6, 3,7,6,6);
+	cityClass Edinburgh("United Kingdom", "English", 495360, 8, 4, 7, 7, 6);
+	cityClass Dublin("Ireland", "English", 527612, 4, 4, 8, 7, 6);
+	cityClass Budapest("Hungary", "Hungarian", 1732000, 10, 6, 8, 7, 8);
+	cityClass Brussels("Belgium", "Dutch", 177307, 4, 5, 8, 6, 7);
+	cityClass Bruges("Belgium", "Flemish", 117377, 7, 2, 7, 5, 8);
+	cityClass Barcelona("Spain", "Spanish", 1602000, 4, 7, 6, 7);
+	cityClass Berlin("Germany", "German", 3502000, 7, 5, 9, 10, 7);
+	cityClass Amsterdam("Netherlands", "Dutch", 779808, 3, 8, 7, 8);
+
 
 	while (answer==0){
 		cout << "Welcome to our travel app! What would you like to do today? " << endl;
