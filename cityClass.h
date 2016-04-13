7df34331c39491fd73343b93ae3e0dfe81b26f94
Kimberly Kosman <kkosman1@nd.edu>
2016-04-13 15:27:25 -0400

    Array of classes

--- cityClass.h
+++ cityClass.h
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <string>
 using namespace std;
 
 class cityClass {
--- main.cpp
+++ main.cpp
@@ -2,37 +2,41 @@
 #include <string>
 #include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main(){
 	int answer=0; //customer choice for the program
 	int country=5;
 	//All of the instantiations of the city classes
-	cityClass Salzburg("German","Austria",145871, 5, 9, 7, 5, 9);
-	cityClass Vienna("Austria", "German", 1741000, 8, 4, 7, 7, 7);
-	cityClass Venice("Italy", "Italian", 260060, 6,2,6,6,9);
-	cityClass Tuscany("Italy", "Italian", 3753000, 7,3,3,2,9);
-	cityClass StPetersburg("Russia", "Russian", 249688, 3, 5, 8, 8, 8);
-	cityClass Stockholm("Sweden", "Swedish", 789024, 4,5,7,6,7);
-	cityClass Rome("Italy", "Italian", 2626000, 4, 5, 9, 10, 7);
-	cityClass Prague("Czech Republic", "Czech", 1247000, 9,8,7,6,8);
-	cityClass Paris("France", "French", 66030000, 2,6,9,10,9);
-	cityClass Monaco("France", "French", 37831, 5, 6, 5, 3, 6);
-	cityClass Madrid("Spain", "Spanish", 3165000, 8,6, 7, 8,8);
-	cityClass London("United Kingdom", "English", 8539000, 3, 6, 8,7,7);
-	cityClass Lisbon("Portugal", "Portuguese", 530847, 8,7,6,5,7);
-	cityClass Istanbul("Turkey", "Turkish", 14030000, 8,8,8,7,7);
-	cityClass Florence("France", "French", 361679, 6, 3,7,6,6);
-	cityClass Edinburgh("United Kingdom", "English", 495360, 8, 4, 7, 7, 6);
-	cityClass Dublin("Ireland", "English", 527612, 4, 4, 8, 7, 6);
-	cityClass Budapest("Hungary", "Hungarian", 1732000, 10, 6, 8, 7, 8);
-	cityClass Brussels("Belgium", "Dutch", 177307, 4, 5, 8, 6, 7);
-	cityClass Bruges("Belgium", "Flemish", 117377, 7, 2, 7, 5, 8);
-	cityClass Barcelona("Spain", "Spanish", 1602000, 4, 7, 6, 7);
-	cityClass Berlin("Germany", "German", 3502000, 7, 5, 9, 10, 7);
-	cityClass Amsterdam("Netherlands", "Dutch", 779808, 3, 8, 7, 8);
+	cityClass Salzburg("Salzburg", "German","Austria",145871, 5, 9, 7, 5, 9);
+	cityClass Vienna("Vienna", "Austria", "German", 1741000, 8, 4, 7, 7, 7);
+	cityClass Venice("Venice", "Italy", "Italian", 260060, 6,2,6,6,9);
+	cityClass Tuscany("Tuscany", "Italy", "Italian", 3753000, 7,3,3,2,9);
+	cityClass StPetersburg("St. Petersburg", "Russia", "Russian", 249688, 3, 5, 8, 8, 8);
+	cityClass Stockholm("Stockholm", "Sweden", "Swedish", 789024, 4,5,7,6,7);
+	cityClass Rome("Rome", "Italy", "Italian", 2626000, 4, 5, 9, 10, 7);
+	cityClass Prague("Prague", "Czech Republic", "Czech", 1247000, 9,8,7,6,8);
+	cityClass Paris("Paris", "France", "French", 66030000, 2,6,9,10,9);
+	cityClass Monaco("Monaco", "France", "French", 37831, 5, 6, 5, 3, 6);
+	cityClass Madrid("Madrid", "Spain", "Spanish", 3165000, 8,6, 7, 8,8);
+	cityClass London("London", "United Kingdom", "English", 8539000, 3, 6, 8,7,7);
+	cityClass Lisbon("Lisbon", "Portugal", "Portuguese", 530847, 8,7,6,5,7);
+	cityClass Istanbul("Istanbul", "Turkey", "Turkish", 14030000, 8,8,8,7,7);
+	cityClass Florence("Florence", "France", "French", 361679, 6, 3,7,6,6);
+	cityClass Edinburgh("Edinburgh", "United Kingdom", "English", 495360, 8, 4, 7, 7, 6);
+	cityClass Dublin("Dublin", "Ireland", "English", 527612, 4, 4, 8, 7, 6);
+	cityClass Budapest("Budapest", "Hungary", "Hungarian", 1732000, 10, 6, 8, 7, 8);
+	cityClass Brussels("Brussels", "Belgium", "Dutch", 177307, 4, 5, 8, 6, 7);
+	cityClass Bruges("Bruges", "Belgium", "Flemish", 117377, 7, 2, 7, 5, 8);
+	cityClass Barcelona("Barcelona", "Spain", "Spanish", 1602000, 4, 7, 6, 7);
+	cityClass Berlin("Berlin", "Germany", "German", 3502000, 7, 5, 9, 10, 7);
+	cityClass Amsterdam("Amsterdam", "Netherlands", "Dutch", 779808, 3, 8, 7, 8);
 
+	// array of cities
+	cityClass * Cities[23] = {Salzburg, Vienna, Venice, Tuscany, StPetersburg, Stockholm, Rome, Prague, Paris, Monaco, Madrid, London, Lisbon, Istanbul, Florence, Edinburgh, Dublin, Budapest, Brussels, Bruges, Barcelona, Berlin, Amsterdam};
 
+	//prompts for options
 	while (answer==0){
 		cout << "Welcome to our travel app! What would you like to do today? " << endl;
 		cout << "1) Enter in a bucket list and we will match as many items as we can " << endl;
